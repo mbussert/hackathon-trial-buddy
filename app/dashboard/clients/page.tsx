b8a54c2b7354a0dd3fd1fd8874e701a209e126a6
Michael Bussert <mbussert@gmail.com>
2024-07-07 09:54:50 -0400

    Add client page and data table

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -1,5 +1,42 @@
 'use server'
 
+import { ClientTable } from './client-table'
+import { columns, Payment } from './client-columns'
+
+import { Button } from '@/components/ui/button'
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '3242efa',
+      amount: 200,
+      status: 'success',
+      email: 'ooga@booga.com',
+    },
+  ]
+}
+
 export default async function ClientsPage() {
-  return <section className="flex-1 p-4 md:p-6">Clients</section>
+  const data = await getData()
+
+  return (
+    <section className="flex-1 p-4 md:p-6">
+      <header className="flex items-center justify-between pb-4 border-b">
+        <div>
+          <h1 className="text-2xl font-bold">Clients</h1>
+        </div>
+        <Button>New Client</Button>
+      </header>
+      <div className="mt-6">
+        <ClientTable columns={columns} data={data} />
+      </div>
+    </section>
+  )
 }
